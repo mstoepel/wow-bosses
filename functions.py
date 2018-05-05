6a95f47ae8fae5c47b89904bedb3ac90b0fcd44a
Michael <mstoepel@gmail.com>
2018-05-05 16:42:05 -0400

    init commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+import requests
+
+class Wow:
+    def __init__(self):
+        self.api_key = 'h2dujjpu5aea5999kffnjddsvxjdssrp'
+
+    def get_boss(self, id):
+        url = 'https://us.api.battle.net/wow/boss/{id}?locale=en_US&apikey={api_key}'
+        id = id
+        info = {'id': id, 'api_key': self.api_key}
+
+        return requests.get(url.format(**info)).text
+
+    def get_all_bosses(self):
+        url = 'https://us.api.battle.net/wow/boss/?locale=en_US&apikey={api_key}'
+        info = {'api_key': self.api_key}
+
+        return requests.get(url.format(**info)).json()['bosses']
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from functions import Wow
+
+
+wow = Wow()
+
+b = wow.get_boss(24723)
+c = wow.get_all_bosses()
+
