7f4767e1ba05893949712c4389d34fddf56686d7
mstoepel <mstoepel@gmail.com>
2018-05-05 17:16:40 -0400

    adding spacy

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from functions import Wow
+import spacy
 
-
+nlp = spacy.load('en')
 wow = Wow()
 
 b = wow.get_boss(24723)
-c = wow.get_all_bosses()
+# c = wow.get_all_bosses()
+
+text = b['description']
+
+doc = nlp(text)
+
+for i in range(len(doc.ents)):
+    print('{} is a {}'.format(doc.ents[i], doc.ents[i].label_))
+
+
 
